d170e8beeefcc4e6627c9215540f885712ea6e0e
jonjecker <jonjecker@gmail.com>
2015-09-01 20:20:47 -0700

    todolist fixed

---
+++ week-6/comment_page.js
@@ -0,0 +1,17 @@
+// Click the like button
+// housed in the div class "like"
+// reach in the like-count element
+// grab that number
+// parse as an integer
+// increase by one
+// insert the number back in
+//
+//
+// comment field
+// Take the input in the comment field form
+// when the button submits the form
+// take the value of the textarea
+// set it to the text content of the new area
+// once it is createded, append it to the new comment area
+//
+// **When you click button you will need to do an event.preventDefault()
