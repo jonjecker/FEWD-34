4712dea06fce2ff3aa302e022891e81a09c20499
jonjecker <jonjecker@gmail.com>
2015-09-27 18:08:40 -0700

    making the final project happen

---
+++ bookmarkletONE.js
@@ -0,0 +1,101 @@
+
+function myFunction() {
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Cloud/g, "Privacy-free");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/cloud/g, "privacy-free");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Software/g, "Something that runs on your computer");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/software/g, "something that runs on your computer");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/evangalist/g, "guy who talks too much");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Evangalist/g, "Guy who talks too much");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Disruptive/g, "Midly interesting");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/disruptive/g, "mildly interesting");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Revolutionary/g, "Kinda new");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/revolutionary/g, "kinda new");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Game/g, "Niche");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/game/g, "niche");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Changer/g, "Exploiter");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/changer/g, "exploiter");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Changing/g, "Exploiting");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/changing/g, "exploiting");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Innovative/g, "A riff on something someone did before");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/innovative/g, "a riff on something someone did before");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Fresh/g, "Figuratively, not physically fresh");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/fresh/g, "figuratively, not physically fresh");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/SaaS/g, "a product you have to pay for over and over");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/CRM/g, "a computer thing you won't understand unless you're in sales");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/CRMs/g, "computer things you won't understand unless you're in sales");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/Steve Jobs/g, "That one CEO that Ashton Kutcher played");
+  document.querySelector("p").innerHTML = res;
+
+  var str = document.querySelector("p").innerHTML;
+  var res = str.replace(/growth hacker/gi, "marketer without a budget");
+  document.querySelector("p").innerHTML = res;
